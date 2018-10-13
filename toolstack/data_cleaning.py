49821eb78d4b9e281522922df24f6af4a55be2fa
unknown <anagharumade.8@gmail.com>
2018-10-13 13:33:50 -0400

    Function for removing outliers

---
+++ toolstack/data_cleaning.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+from numpy import percentile
+
+def remove_outliers(df, column, filter_type):
+    
+    """ 
+    Removes outliers from a numerical columns in a dataframe
+    
+    Parameters
+    ----------
+    df : DataFrame
+        The df to perform case operation on.
+    column : float
+        The column selected should be present in the Dataframe passed
+    filter_type : string, default 'english'
+        Options: 'standard_deviation', 'IQR'
+
+    
+    Returns
+    -------
+    List
+    
+    """
+    
+    values = np.array(df[column])
+    
+    if filter_type == 'standard_deviation':
+        col_mean, col_std = values.mean(), values.std()
+        limit = col_std * 3
+        
+        lower, upper = col_mean - limit, col_mean + limit
+        
+        i=0
+        outliers_removed = []
+        for value in values:
+            if value >= lower and value <= upper:
+                outliers_removed.append(value)
+                #indexes.append(i)
+                i = i+1
+                
+    if filter_type == 'IQR':
+        Quar25, Quar75 = percentile(values, 25), percentile(values, 75)
+        IQR = Quar75 - Quar25
+        limit = IQR * 1.5
+        
+        lower, upper = Quar25 - limit, Quar75 + limit
+        
+        i=0
+        outliers_removed = []
+        for value in values:
+            if value >= lower and value <= upper:
+                outliers_removed.append(value)
+                #indexes.append(i)
+                i = i+1
+    
+    return outliers_removed
